c9644152294188039cf1e468462fec48364aae44
Fl0jfk <florian.hacqueville@hotmail.fr>
2023-11-12 12:26:48 +0100

    fix meats section

--- src/app/components/meats/Meats.tsx
+++ src/app/components/meats/Meats.tsx
@@ -2,6 +2,7 @@
 
 import { useData } from "@/app/contexts/data";
 
+
 function Meats(){
     const data = useData();
     return (
@@ -10,7 +11,7 @@ function Meats(){
             <div className="flex sm:flex-col gap-4 w-full items-center">
                 {data.meats.map((meat:{id:number, name:string, halal:boolean, description:string, image:string}) => (
                     <div key={meat.id} className="flex rounded-xl flex-col w-full" style={{backgroundImage: `url(${meat.image})`, backgroundSize: "cover", backgroundPosition: "50%, 50%"}}>
-                        <div className="flex flex-col justify-between bg-gray-900 opacity-80 gap-8 p-4 w-full h-full min-h-[400px] sm:min-h-[300px] rounded-xl">
+                        <div className="flex flex-col justify-between bg-gray-900 opacity-80 gap-8 p-4 w-full h-full min-h-[400px] sm:min-h-[300px] rounded-xl hover:opacity-0 touch:opacity-0 cursor-pointer">
                             <h3 className="text-3xl">{meat.name}</h3>
                             <p>{meat.description}</p>
                             <p className="text-sm">{meat.halal ? "Ce plat est halal" : "Ce plat n'est pas halal"}</p>
