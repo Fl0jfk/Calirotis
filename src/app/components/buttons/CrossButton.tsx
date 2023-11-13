948ab88b08d2d9ecbde4efe35638cf8351a8ee28
Fl0jfk <florian.hacqueville@hotmail.fr>
2023-11-13 12:29:23 +0100

    delete a css files

--- src/app/components/buttons/CrossButton.tsx
+++ src/app/components/buttons/CrossButton.tsx
@@ -8,7 +8,7 @@ function CrossButton({menuOpened}: CrossButton ){
     const clickOnButton = () => {
         setButtonGo(!buttonGo);
     }
-    const hambOrCross = (buttonGo ? "menu ": "menu opened fixed z-[10]");
+    const hambOrCross = (buttonGo ? "cursor-pointer border-0 p-0 z-[1000] ": "cursor-pointer border-0 p-0 z-[1000] opened fixed z-[10]");
     useEffect(() => {
         if(menuOpened === false){
             setButtonGo(true);
@@ -17,9 +17,9 @@ function CrossButton({menuOpened}: CrossButton ){
     return (
         <button className={`${hambOrCross} md:flex sm:flex lg:hidden xl:hidden`} onClick={clickOnButton} type="button" aria-label="Aria Cross or Hamburger">
             <svg width="40" height="40" viewBox="0 0 100 100">
-              <path className="line line1" d="M 20,29.000046 H 80.000231 C 80.000231,29.000046 94.498839,28.817352 94.532987,66.711331 94.543142,77.980673 90.966081,81.670246 85.259173,81.668997 79.552261,81.667751 75.000211,74.999942 75.000211,74.999942 L 25.000021,25.000058"/>
-              <path className="line line2" d="M 20,50 H 80"/>
-              <path className="line line3" d="M 20,70.999954 H 80.000231 C 80.000231,70.999954 94.498839,71.182648 94.532987,33.288669 94.543142,22.019327 90.966081,18.329754 85.259173,18.331003 79.552261,18.332249 75.000211,25.000058 75.000211,25.000058 L 25.000021,74.999942"/>
+              <path className="line1 fill-none stroke-white stroke-6" style={{transition: "stroke-dasharray 600ms cubic-bezier(0.4, 0, 0.2, 1), stroke-dashoffset 600ms cubic-bezier(0.4, 0, 0.2, 1)"}} d="M 20,29.000046 H 80.000231 C 80.000231,29.000046 94.498839,28.817352 94.532987,66.711331 94.543142,77.980673 90.966081,81.670246 85.259173,81.668997 79.552261,81.667751 75.000211,74.999942 75.000211,74.999942 L 25.000021,25.000058"/>
+              <path className="line2 fill-none stroke-white stroke-6" style={{transition: "stroke-dasharray 600ms cubic-bezier(0.4, 0, 0.2, 1), stroke-dashoffset 600ms cubic-bezier(0.4, 0, 0.2, 1)"}} d="M 20,50 H 80"/>
+              <path className="line3 fill-none stroke-white stroke-6" style={{transition: "stroke-dasharray 600ms cubic-bezier(0.4, 0, 0.2, 1), stroke-dashoffset 600ms cubic-bezier(0.4, 0, 0.2, 1)"}} d="M 20,70.999954 H 80.000231 C 80.000231,70.999954 94.498839,71.182648 94.532987,33.288669 94.543142,22.019327 90.966081,18.329754 85.259173,18.331003 79.552261,18.332249 75.000211,25.000058 75.000211,25.000058 L 25.000021,74.999942"/>
             </svg>
         </button>
     )
--- src/app/components/carousel/Carousel.tsx
+++ src/app/components/carousel/Carousel.tsx
@@ -2,7 +2,6 @@
 
 import { useState } from "react";
 import Image from "next/image";
-import "./carousel.css";
 
 function Carousel ({ data }:CarouselProps){
     const [slide, setSlide] = useState(0);
@@ -25,10 +24,10 @@ function Carousel ({ data }:CarouselProps){
             <svg width="50px" height="50px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" onClick={nextSlide} className="absolute right-[1em] cursor-pointer bg-black opacity-50 rounded-full">
                 <path d="M5 12H19M19 12L13 6M19 12L13 18" stroke="#fff" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
             </svg>
-            <span className="indicators">
+            <span className="flex absolute bottom-[1rem]">
                 {data.map((_, idx) => {
                     return (
-                        <button key={idx} id="al" name="boutons de slider" aria-label="boutons de slider" className={slide === idx ? "indicator" : "indicator indicator-inactive"} onClick={() => setSlide(idx)}></button>
+                        <button key={idx} id={`al${idx}`} name="boutons de slider" aria-label="boutons de slider" className={slide === idx ? "bg-white h-[0.8em] w-[0.8em] rounded-full border-0 outline-none shadow-gray-300 shadow-lg ml-[0.2rem] mr-[0.2rem] cursor-pointer" : "h-[0.8em] w-[0.8em] rounded-full border-0 outline-none shadow-gray-300 shadow-lg ml-[0.2rem] mr-[0.2rem] cursor-pointer bg-gray-500"} onClick={() => setSlide(idx)}></button>
                     );
                 })}
             </span>
