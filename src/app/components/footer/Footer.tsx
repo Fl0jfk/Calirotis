3eb001680455bfa299daaeb278339a68c662810b
Fl0jfk <florian.hacqueville@hotmail.fr>
2023-11-13 14:12:18 +0100

    change img bbq

--- src/app/components/footer/Footer.tsx
+++ src/app/components/footer/Footer.tsx
@@ -1,7 +1,8 @@
 function Footer(){
     return (
         <footer className="p-4">
-            <p className="text-xl text-center">&copy; 2023 Calirôtis</p> 
+            <p className="text-center">Le n°1 du cochon de lait en Normandie</p>
+            <p className="text-center">&copy; 2023 Calirôtis</p> 
         </footer>
     )
 }
