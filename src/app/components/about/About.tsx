f8607519b9e5b780c94e49f56039f2990f56bbb9
Fl0jfk <florian.hacqueville@hotmail.fr>
2024-01-08 18:48:33 +0100

    chnge txt

--- src/app/components/about/About.tsx
+++ src/app/components/about/About.tsx
@@ -4,14 +4,12 @@ function About (){
     return (
         <section className="w-full flex flex-col p-6 items-center gap-8" id="about">
             <h2 className="text-6xl">À propos</h2>
-            <div className="flex-col w-full flex items-center rounded-t-xl h-[500px] max-w-[1000px]">
-                <div className="w-full h-[55%] sm:h-[40%] relative">
-                    <Image src="https://calirotis.s3.eu-west-3.amazonaws.com/assets/img/photo+de+profil.webp" alt="Photo de profil" fill className="rounded-t-xl relative" style={{objectFit : "cover", objectPosition: "50% 28%"}}/>
-                </div>
-                <div className="w-full h-[45%] sm:h-[60%] p-8 flex flex-col items-center text-xl bg-gray-700 rounded-b-xl overflow-y-scroll">
-                    <p className="text-start w-full">Je suis Martin après plusieurs années en tant que traiteur au sein d&apos;une entreprise.</p>
-                    <p className="text-start w-full">J&apos;ai décidé de lancer ma propre affaire, je me suis spécialisé dans la rotisserie.(cochon de lait, mechoui, barbecue géant...)</p>
-                    <p className="text-start w-full">J&apos;accorde une importance primordiale à la qualité de nos produits et services. Je sélectionne soigneusement les ingrédients les plus frais et les plus savoureux. Chaque plat est préparé avec expertise et présenté avec soin pour vous offrir la meilleure expérience possible.</p>
+            <div className="flex-col w-full flex items-center rounded-xl h-full max-w-[1000px]">
+                <div className="w-full h-full p-8 flex flex-col items-center text-xl bg-gray-700 rounded-xl">
+                    <p className="text-start w-full">Je suis Martin. Après x années à travailler chez un traiteur j&apos;ai décidé de lancer mon entreprise en me spécialisant dans la rôtisserie (cochon de lait, méchoui, barbecue géant...).</p><br/>
+                    <p className="text-start w-full">J&apos;accorde une grande importance au choix des ingrédients que je choisi frais et savoureux. Chaque plat sera préparé avec attention et présenté avec soin pour vous offrir le meilleur moment possible.</p><br/>
+                    <p className="text-start w-full">J&apos;accorde une importance primordiale à la qualité de nos produits et services. Je sélectionne soigneusement les ingrédients les plus frais et les plus savoureux. Chaque plat est préparé avec expertise et présenté avec soin pour vous offrir la meilleure expérience possible.</p><br/>
+                    <p className="text-start w-full">Je mets à votre service mon professionnalisme et ma passion pour vous permettre de profiter sans vous souciez de votre repas.</p>
                 </div>
             </div>
         </section>
@@ -19,3 +17,4 @@ function About (){
 }
 
 export default About;
+
