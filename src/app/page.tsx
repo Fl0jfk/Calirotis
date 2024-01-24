46c2f03d0bbb549270cdb3356b96784abba25256
Fl0jfk <florian.hacqueville@hotmail.fr>
2024-01-24 15:19:26 +0100

    add google tag

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,6 +13,7 @@ export default function Home() {
     <>
       <Head>
         <title>Page d`&apos;`accueil de Calirotis</title>
+        <meta name="google-site-verification" content="6Z0b5nwln-gOrQkGTIQtXxHo1bdASiGnvCi1pnpu6oA" />
       </Head>
       <DataProvider>
         <Header/>
