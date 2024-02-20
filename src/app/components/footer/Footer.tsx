5063dc4569b2f3b29f0641c2a08ddeac990f09be
Fl0jfk <florian.hacqueville@hotmail.fr>
2024-02-20 11:30:49 +0100

    change name

--- src/app/components/footer/Footer.tsx
+++ src/app/components/footer/Footer.tsx
@@ -2,7 +2,7 @@ function Footer(){
     return (
         <footer className="p-4">
             <p className="text-center">L&apos;expert du cochon de lait en Normandie</p>
-            <p className="text-center">&copy; 2024 Calirôtis</p> 
+            <p className="text-center">&copy; 2024 Calirotis</p> 
         </footer>
     )
 }
--- src/app/components/header/Header.tsx
+++ src/app/components/header/Header.tsx
@@ -38,7 +38,7 @@ function Header(){
                         <Image src={data.profile.logo} alt='Logo de calirotis' width={50} height={50} className='cursor-pointer z-[8] mb-[5px]' onClick={()=>{window.scrollTo({top:0, left:0, behavior:'smooth'})}}/>
                     }
                 </div>
-                <h1 className='text-center w-full text-xl sm:block hidden'>Calirôtis</h1>
+                <h1 className='text-center w-full text-xl sm:block hidden'>Calirotis</h1>
                 <div className='w-10/12 sm:w-2/12 flex justify-end items-center sm:mt-[-5px]'>
                     <Navbar menuOpened={menuOpened} onLinkClick={handleLinkClick}/>
                     <div className='flex justify-end w-[40] h-[100px] md:h-[50px] sm:h-[30px] items-center' onClick={() => handleClick()}>
